9198880183329b148609894c29234241b788cd1c
davidsosagomez <sosagomezdavid@gmail.com>
2023-11-24 18:49:06 +0100

    with actualiced DH param

--- signal_interface/scripts/DK_node.py
+++ signal_interface/scripts/DK_node.py
@@ -15,10 +15,9 @@ class ForwardKinematics:
         self.jointstate_subscriber = rospy.Subscriber("/joint_states", JointState, self.jointstate_callback)
         self.tf_matrix_pub = rospy.Publisher("/tf_matrix", Float64MultiArray, queue_size = 10)
         self.pose_pub = rospy.Publisher("/end_effector_pose", Pose, queue_size = 10)
-        #parameters (taken form https://frankaemika.github.io/docs/control_parameters.html#denavithartenberg-parameters)
-        self.alpha = np.array([0, -np.pi/2, np.pi/2, np.pi/2, -np.pi/2, np.pi/2, np.pi/2])
-        self.a = np.array([0, 0, 0, 0.0825, -0.0825, 0, 0.088])
-        self.d = np.array([0.333, 0, 0.316, 0, 0.384, 0, 0.107])
+        self.alpha = np.array([np.pi/2, np.pi/2, -np.pi/2, -np.pi/2, 0, 0])
+        self.a = np.array([0, 0.086, 0.2195, 0.2207, 0.2695, 0.17632])
+        self.d = np.array([0.11285, 0.14106, 0.0693, 0.0692, 0.0133, 0.0202])
 
     def jointstate_callback(self, msg):
         self.q = np.array(msg.position, dtype = np.float64)
@@ -31,9 +30,9 @@ class ForwardKinematics:
 
     def dh_between_frames(self, q, a, alpha, d):
         #general formula for transformation between two frames
-        tf_matrix_between_frames = np.array([[np.cos(q), -np.sin(q), 0, a],
-                                          [np.sin(q)*np.cos(alpha), np.cos(q)*np.cos(alpha), -np.sin(alpha), -np.sin(alpha)*d],
-                                          [np.sin(q)*np.sin(alpha), np.cos(q)*np.sin(alpha), np.cos(alpha), np.cos(alpha)*d],
+        tf_matrix_between_frames = np.array([[np.cos(q), -np.cos(alpha)*np.sin(q), np.sin(alpha)*np.sin(q), a*np.cos(q)],
+                                          [np.sin(q), np.cos(q)*np.cos(alpha), -np.sin(alpha)*np.cos(q), a*np.sin(q)],
+                                          [0, np.sin(alpha), np.cos(alpha), d],
                                           [0, 0, 0, 1]])
         return tf_matrix_between_frames
 
@@ -41,22 +40,22 @@ class ForwardKinematics:
         
         for i in range(len(q)):
             if i == 0:
-                T_0_7 = self.dh_between_frames(q[i], self.a[i], self.alpha[i], self.d[i])
+                T_0_6 = self.dh_between_frames(q[i], self.a[i], self.alpha[i], self.d[i])
             else:
-                T_0_7 = np.dot(T_0_7, self.dh_between_frames(q[i], self.a[i], self.alpha[i], self.d[i]))
+                T_0_6 = np.dot(T_0_6, self.dh_between_frames(q[i], self.a[i], self.alpha[i], self.d[i]))
                     
         #Converting tf_matrix to position and orientation to publish as a pose type msg 
         # https://answers.ros.org/question/379109/transformation-matrices-to-geometry_msgspose/
-        q = tf.transformations.quaternion_from_matrix(T_0_7)
+        q = tf.transformations.quaternion_from_matrix(T_0_6)
         pose = Pose()
-        pose.position.x = T_0_7[0, 3]
-        pose.position.y = T_0_7[1, 3]
-        pose.position.z = T_0_7[2, 3]
+        pose.position.x = T_0_6[0, 3]
+        pose.position.y = T_0_6[1, 3]
+        pose.position.z = T_0_6[2, 3]
         pose.orientation.x = q[0]
         pose.orientation.y = q[1]
         pose.orientation.z = q[2]
         pose.orientation.w = q[3]
-        return pose, T_0_7
+        return pose, T_0_6
 
 if __name__ == "__main__":
     rospy.init_node("forward_kinematics")
