f7021c54efe1fae667a9f3dc7d2dc2aa6b4c58e1
davidsosagomez <sosagomezdavid@gmail.com>
2023-12-01 16:18:42 +0100

    reshaaping pred. matrix & print statements

--- signal_interface/scripts/collision_detector.py
+++ signal_interface/scripts/collision_detector.py
@@ -6,9 +6,12 @@ import numpy as np
 class CollisionDetector:
     def __init__(self):
         # Define the predefined matrix for comparison
-        self.predefined_matrix = np.array([[0.95085546, 0.29124849, 0.10511047, 0.301],
-                                           [0.19840159, -0.83371212, 0.51532603, -0.139],
-                                           [0.2377198, -0.46914648, -0.85052388, 0.266]]).T.reshape(-1, 1)
+        self.predefined_matrix = np.array([[-0.55, -0.53, -0.65, -0.39],
+                                           [-0.83, 0.45, 0.34, -0.03],
+                                           [ 0.11, 0.72, -0.68, 0.47],
+                                           [ 0.0, 0.0, 0.0, 1.0]])
+        print("Predefined collision matrix: ")
+        print(self.predefined_matrix)
 
         # ROS Node and Publisher setup
         rospy.init_node('collision_detector', anonymous=True)
@@ -19,7 +22,8 @@ class CollisionDetector:
 
     def callback(self, msg):
         # Convert data to numpy array
-        current_matrix = np.array(msg.data).T.reshape(-1, 1)
+        current_matrix = np.round(np.array(msg.data).reshape(4, 4),2)
+        
 
         # Compare matrices and publish collision status
         if np.array_equal(current_matrix, self.predefined_matrix):
