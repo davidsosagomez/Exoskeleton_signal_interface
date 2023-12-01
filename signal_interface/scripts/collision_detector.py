d488752e68ec1afead27b7ca9dcacd1c062bbe8b
davidsosagomez <sosagomezdavid@gmail.com>
2023-12-01 12:41:47 +0100

    coments

--- signal_interface/scripts/collision_detector.py
+++ signal_interface/scripts/collision_detector.py
@@ -8,7 +8,7 @@ class CollisionDetector:
         # Define the predefined matrix for comparison
         self.predefined_matrix = np.array([[0.95085546, 0.29124849, 0.10511047, 0.301],
                                            [0.19840159, -0.83371212, 0.51532603, -0.139],
-                                           [0.2377198, -0.46914648, -0.85052388, 0.266]]).T.reshape(-1, 1)  # Fill in with your predefined matrix values
+                                           [0.2377198, -0.46914648, -0.85052388, 0.266]]).T.reshape(-1, 1)
 
         # ROS Node and Publisher setup
         rospy.init_node('collision_detector', anonymous=True)
