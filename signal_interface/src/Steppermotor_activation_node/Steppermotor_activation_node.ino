817a28edb7e432774fd132f855aabf671b776bf4
davidsosagomez <sosagomezdavid@gmail.com>
2023-12-11 18:44:53 +0100

    stepper as a node

--- signal_interface/src/Steppermotor_activation_node/Steppermotor_activation_node.ino
+++ signal_interface/src/Steppermotor_activation_node/Steppermotor_activation_node.ino
@@ -1,62 +1,57 @@
-// Define pin connections & motor's steps per revolution
+#include <ros.h>
+#include <std_msgs/Bool.h>
+
 const int dirPin = 5;
 const int stepPin = 2;
 const int stepsPerRevolution = 200;
 
-// Variables to keep track of the last command and its state
 bool lastCommandWasTrue = false;
 bool motorMovedSinceLastFalse = false;
 
-void setup() {
-    // Initialize Serial communication
-    Serial.begin(9600);
+ros::NodeHandle nh;
 
-    // Declare pins as Outputs
-    pinMode(stepPin, OUTPUT);
-    pinMode(dirPin, OUTPUT);
-}
+void collisionCallback(const std_msgs::Bool& msg) {
+    bool input = msg.data;
 
-void loop() {
-    if (Serial.available() > 0) {
-        // Read the incoming string from Serial Monitor
-        String input = Serial.readStringUntil('\n');
-
-        // Clear the Serial buffer to avoid processing accumulated commands
-        while(Serial.available() > 0) {
-            Serial.read();
+    // Check if the input is true or false
+    if (input) {
+        if (!motorMovedSinceLastFalse) {
+            digitalWrite(dirPin, LOW);
+            rotateMotor(15);
+            motorMovedSinceLastFalse = true;
         }
-
-        // Check if the input is 'true' or 'false'
-        if (input.equals("true")) {
-            if (!motorMovedSinceLastFalse) {
-                // Set motor direction counter-clockwise
-                digitalWrite(dirPin, LOW);
-                rotateMotor(15); // Rotate 10 steps
-                motorMovedSinceLastFalse = true;
-            }
-            lastCommandWasTrue = true;
-        } else if (input.equals("false")) {
-            // Set motor direction clockwise and rotate if last command was 'true'
-            if (lastCommandWasTrue) {
-                digitalWrite(dirPin, HIGH);
-                rotateMotor(15); // Rotate 10 steps
-            }
-            lastCommandWasTrue = false;
-            motorMovedSinceLastFalse = false;
+        lastCommandWasTrue = true;
+    } else {
+        if (lastCommandWasTrue) {
+            digitalWrite(dirPin, HIGH);
+            rotateMotor(15);
         }
-
-        // A short delay to debounce the input and allow the motor to settle
-        delay(100);
+        lastCommandWasTrue = false;
+        motorMovedSinceLastFalse = false;
     }
 }
 
+ros::Subscriber<std_msgs::Bool> collision_sub("/collision", &collisionCallback);
+
+void setup() {
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    nh.initNode();
+    nh.subscribe(collision_sub);
+}
+
 void rotateMotor(int steps) {
-    // Spin motor for the specified number of steps
     for(int x = 0; x < steps; x++) {
         digitalWrite(stepPin, HIGH);
         delayMicroseconds(1200);
         digitalWrite(stepPin, LOW);
         delayMicroseconds(1200);
     }
-    delay(1000); // Wait a second after rotation
+    delay(1000);
 }
+
+void loop() {
+    nh.spinOnce();
+    delay(10);
+}
+
