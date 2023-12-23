fbe03285c038a32ad4d31f2cf569d0ab72077916
davidsosagomez <sosagomezdavid@gmail.com>
2023-12-23 04:08:08 +0100

    in original Right hand kos

--- signal_interface/scripts/pose_matrix_converter.py
+++ signal_interface/scripts/pose_matrix_converter.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import rospy
 import tf
+import numpy as np
 from unity_robotics_demo_msgs.msg import PosRot
 from std_msgs.msg import Float64MultiArray
 
 def pose_callback(pose_msg):
     # Extract position and quaternion from the unity costumize pose message
-    position = (pose_msg.pos_x, pose_msg.pos_y, pose_msg.pos_z)
+    position = (pose_msg.pos_z, -pose_msg.pos_x, pose_msg.pos_y)
     quaternion = (pose_msg.rot_x, pose_msg.rot_y, pose_msg.rot_z, pose_msg.rot_w)
 
-    # Convert quaternion and position to transformation matrix
+    print("quaternion: ", quaternion)
+
+
+    print("Position (x, y, z): ", position)
+
+    # Convert the rotated quaternion and position to a transformation matrix
+    #transformation_matrix = tf.transformations.quaternion_matrix(rotated_quaternion)
     transformation_matrix = tf.transformations.quaternion_matrix(quaternion)
     transformation_matrix[0][3] = position[0]
     transformation_matrix[1][3] = position[1]
@@ -21,7 +28,7 @@ def pose_callback(pose_msg):
 
     # Publish the transformation matrix
     pub.publish(matrix_msg)
-
+    
 if __name__ == '__main__':
     rospy.init_node('pose_matrix_converter')
 
